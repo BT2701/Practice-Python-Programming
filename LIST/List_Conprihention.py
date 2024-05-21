df00769874be8772f4c705c5323a4178ce9eb0a4
Duong Thanh Truong <truongnopro1111@gmail.com>
2024-05-21 19:06:59 +0700

    day 2: list conprihention

--- LIST/List_Conprihention.py
+++ LIST/List_Conprihention.py
@@ -1,15 +1,19 @@
 widths= [1,3,6,1,7,9]
 heights= [1,6,3,2,9,0]
 weights= [1,2,4,5,9,8]
-def dienTich(width, height):
-    return width * height
+def dienTich(width, weight):
+    return width * weight
 
-def chuVi(width, height):
-    return (width+height)/2
+def chuVi(width, weight):
+    return (width+weight)/2
 def theTich(width, height, weight):
     return width*height*weight
 
+print('Chiều dài: ', [width for width in widths])
+print('Chiều cao: ', [height for height in heights])
+print ('Chiều rộng: ', [weight for weight in weights])
+print('=======================')
 # sở dĩ dùng [] là để đánh dấu đó là một list các diện tích/ chu vi/ thể tích
-print('Diện tích: ',[dienTich(width, height) for width, height in zip(widths, heights)])
-print( 'Chu vi: ',[chuVi(width, height) for width, height in zip (widths, heights)])
+print('Diện tích: ',[dienTich(width, weight) for width, weight in zip(widths, weights)])
+print( 'Chu vi: ',[chuVi(width, weight) for width, weight in zip (widths, weights)])
 print('Thể tích: ',[theTich(width, height, weight) for width, height, weight in zip(widths, heights, weights)]) 
